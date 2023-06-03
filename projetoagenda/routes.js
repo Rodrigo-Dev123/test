e3326779f95e5e8f7482657063224f656e24932f
Rodrigo-Dev123 <rodrigojsg9@gmail.com>
2023-06-03 15:35:49 -0300

    new file

--- projetoagenda/routes.js
+++ projetoagenda/routes.js
@@ -12,11 +12,13 @@ route.get('/', homeController.index);
 // Rotas de login
 route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
-route.post('/login/login', loginController.login);
+route.post('/login/login', loginController.login); 
 route.get('/login/logout', loginController.logout);
 
 // Rotas de contato
 route.get('/contato/index', loginRequired, contatoController.index);
 route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
+// route.post('/contato/edit/:id', loginRequired, contatoController.edit);
 
 module.exports = route;
--- projetoagenda/src/controllers/contatoController.js
+++ projetoagenda/src/controllers/contatoController.js
@@ -1,8 +1,10 @@
 const Contato = require('../models/ContatoModel');
 
 exports.index = (req, res) => {
-    res.render('contato');
-}
+    res.render('contato', {
+        contato: {}
+    });
+};
 
 exports.register = async (req, res) => {
     try {
@@ -24,3 +26,12 @@ exports.register = async (req, res) => {
     }
 
 };
+
+exports.editIndex = async function (req, res) {
+    if (!req.params.id) return res.render('404');
+
+    const contato = await Contato.buscaPorId(req.params.id);
+    if (!contato) return res.render('404');
+
+    res.render('contato', { contato });
+}
